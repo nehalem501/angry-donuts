7efaa5cff322eabd522285636219c6710487e438
nehalem501 <tomek.lecocq@hotmail.fr>
2017-02-16 17:01:42 +0100

    update main

--- src/angry-donuts.cpp
+++ src/angry-donuts.cpp
@@ -1,8 +1,14 @@
 /* main.cpp */
 
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Error: missing argument" << std::endl;
+        std::cout << "Usage: angry-donuts /path/to/data" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
