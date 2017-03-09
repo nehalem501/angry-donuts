a68a4724ebac04ee109e793a46cda7ae056a9c6f
navarna <navarna@navarna>
2017-03-09 21:20:21 +0100

    getPath2 () qui transforme - et \ dans le path Pas tester

--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,7 @@
 #include <uuid/uuid.h>
 
 std::string get_path(uuid_t id, std::string directory);
+std::string get_path2(uuid_t id, std::string directory);
 void create_path(std::string root_path, std::string object_path);
 
 #endif // UTILS_H
