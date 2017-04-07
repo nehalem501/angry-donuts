a6f54e2c0168715d00f88b86e8a3417264bff58f
Kevin Youna <kevin.youna@gmail.com>
2017-04-07 14:49:56 +0200

    version quasi finale d'angry_donuts

--- src/angry-donuts.cpp
+++ src/angry-donuts.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
-#include <regex.h>
+#include <regex>
 #include <unistd.h>
 
 #include "index.h"
@@ -22,10 +22,13 @@ size_t get_file_size(const char * fileName){
   struct stat st;
   if (stat(fileName,&st)){
     fprintf(stderr,"stat sur %s errno : %d \n",fileName,errno);
-  
     exit(EXIT_FAILURE);
   }
   return st.st_size;
+}
+
+bool file_exist(const char * fileName){
+  return ( access( fileName, F_OK ) != -1 );
 }
 
 bool is_dir(const char * fileName){
@@ -37,36 +40,43 @@ bool is_dir(const char * fileName){
     return st.st_mode && S_IFDIR;
 }
 
-void get_uiid_from_string(const char * string,unsigned char * uiid){
-  cout << string << endl;
-  cout << uiid << endl;
-
+void get_uuid_from_string(const char * string,unsigned char * uuid){
   /*
     Il faut tester si l'entree est bien un uiid:
       si ça ne contient pas de slash par exemple ou de caractere speciaux.
+    expression reguliere : [a-zA-Z0-9-]+\g
   */
-  if(uuid_parse(string,uiid) == -1){
+  std::regex rgx("(([a-zA-Z0-9-]{36}))");
+  if(regex_search(string,rgx)){
+    if(uuid_parse(string,uuid) != 0){
         printf("error uuid_parse \n");
         exit(EXIT_FAILURE);
     }
+  }
+  else{
+    cout << "uuid incorrect " << endl;
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char *argv[]) {
   std::cout << "argc "  << argc << std::endl;
-  if(argc==3 || argc ==4 || argc == 5){
+  if(argc ==4 || argc == 5){
     for(int i = 0;i<argc;i++){
       cout << "index " << i << " = " <<   argv[i] << endl;
     }
+ 
     Index index(argv[1]);
     uuid_t uuid;
     char res[32];
     uint64_t length;
     void * mmapped_zone;
+    /*
     if (!is_dir(argv[1])){
       std::cout << "Second argument is not a directory" << std::endl;
       exit(EXIT_FAILURE);
     }
-
+    */
     if (strcmp(argv[2],"put") == 0){
       int src = open(argv[3],'r');
       if(src < 0){
@@ -103,8 +113,15 @@ int main(int argc, char *argv[]) {
           -> met a jour le fichier avec le même uiid
           TOMEK A L'AIDE
         */
-
-
+        /*
+          if(file_exist(argv[4])){
+            
+          }
+          else{
+            perror("le fichier n'existe pas");
+            exit(EXIT_FAILURE);
+          }
+        */
       }
     }
     if(strcmp(argv[2],"get") == 0 ){
@@ -113,7 +130,7 @@ int main(int argc, char *argv[]) {
             angry-donut nom_dossier get uuid
             -> affiche le contenu du fichier
           */
-        get_uiid_from_string(argv[3],uuid);
+        get_uuid_from_string(argv[3],uuid);
         Data data2;
         if (index.get(uuid, &data2) != Status::Success)
           std::cout << "error get" << std::endl;
@@ -144,41 +161,46 @@ int main(int argc, char *argv[]) {
         exit(EXIT_SUCCESS);
       }
     }
+
     if (strcmp(argv[2],"del") == 0 ){
-          if (index.del(uuid) == Status::Success)
-            std::cout << "success on delete!" << std::endl;
-          else
-            std::cout << "delete error..." << std::endl;
-      }
-  }
-  else{
-    if (argc == 2){
-      std::cout << "argv[2]" << std::endl;
+      get_uuid_from_string(argv[3],uuid);
+      if (index.del(uuid) == Status::Success)
+        std::cout << "success on delete!" << std::endl;
+      else
+        std::cout << "delete error..." << std::endl;
     }
+  }
+
+
+  else{
     std::cout << "Error: missing argument" << std::endl;
         //std::cout << "Usage: angry-donuts /path/to/data" << std::endl;
     /*
           angry-donuts nom_dossier put fichier
           -> renvoie l'uiid du nouveau fichier cree dans l'arborescence
         */
-        std::cout << "Usage : angry-donuts /path/to/data commande uuid" << std::endl;
-        std::cout << "common arguments " << std::endl;
-        std::cout << "\tnom_dossier commande" << std::endl;
-        std::cout << "options : put,get,del" << std::endl;
-        std::cout << "\tput option" << std::endl;
-        std::cout << "" << std::endl;
-        std::cout << "\tget option" << std::endl;
-        std::cout <<" angry-donut nom_dossier get uuid"<<std::endl;
-        std::cout << "\taffiche le contenu du fichier" << std::endl;
-        std::cout <<" angry-donut nom_dossier get uuid fichier"<<std::endl;
-        std::cout << "\tcreer un fichier avec le contenu du fichier ayant pour uuid l'uiid donné en paramétre" << std::endl;
+        std::cout << "Usage : angry-donuts /path/to/data commande uuid (fichier)" << std::endl;
+        std::cout << "commande : put,get,del" << std::endl;
         
-
-        std::cout << "\tdel option" << std::endl;
-        std::cout <<"" << std::endl;
-
-        std::cout << "angry-donuts nom_dossier put fichier " << std::endl;
-
+        std::cout << "\toption pour put" << std::endl;
+        
+        std::cout <<"\t\tangry-donut nom_dossier put uuid"<<std::endl;
+        std::cout << "\t\t\taffiche l'uuid du fichier une fois stocké en base" << std::endl;
+        
+        std::cout <<"\t\tangry-donut nom_dossier put uuid fichier"<<std::endl;
+        std::cout << "\t\t\tmet à jour le fichier avec l'uuid uuid avec le fichier (fichier) passé en paramétre" << std::endl;
+        
+        std::cout << "\toption pour get" << std::endl;
+        
+        std::cout <<"\t\tangry-donut nom_dossier get uuid"<<std::endl;
+        std::cout << "\t\t\taffiche le contenu du fichier" << std::endl;
+        
+        std::cout <<"\t\tangry-donut nom_dossier get uuid fichier"<<std::endl;
+        std::cout << "\t\t\tcreer un fichier avec le contenu du fichier ayant pour uuid l'uiid donné en paramétre" << std::endl;
+        
+        std::cout << "\toption pour del" << std::endl;
+        std::cout <<"\t\tangry-donut nom_dossier del uuid" << std::endl;
+        std::cout <<"\t\t\tsupprime le fichier avec l'uuid uuid" << std::endl;
 
         exit(EXIT_FAILURE);
   }
---
+++ src/test/03d91ba3-ef74-49e8-ac03-f726fd346e28
@@ -0,0 +1,12 @@
+/* status.h */
+
+#ifndef STATUS_H
+#define STATUS_H
+
+enum class Status {
+    Success = 0,
+    Error = 1,
+    NoID = 2
+};
+
+#endif // STATUS_H
