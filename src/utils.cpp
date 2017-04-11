b1374ef9d1172844f2eb9010e6697661d07e6f4d
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-04-11 12:25:10 +0200

    bit operations for index refactoring

--- src/utils.cpp
+++ src/utils.cpp
@@ -35,7 +35,7 @@ string get_path2 (uuid_t id , string directory ) {
 
 // 	Split un string avec le charactere donne en argument et renvoie un vecteur 
 //	de string.
-
+/*
 vector<string> splitStringToVector(string str, char char_splitter) {
 	vector<string> vectored_tokens;
 	stringstream myStringAsStream(str); // Turn the string into a stream.
@@ -59,3 +59,42 @@ void create_path(string root_path, string object_path) {
 	}
 }
 // Fin Karim -------------------------------------------------------------------
+*/
+uint8_t byte[] = {
+/* 0b00000001 */ 0x01,
+/* 0b00000010 */ 0x02,
+/* 0b00000100 */ 0x04,
+/* 0b00001000 */ 0x08,
+/* 0b00010000 */ 0x10,
+/* 0b00100000 */ 0x20,
+/* 0b01000000 */ 0x40,
+/* 0b10000000 */ 0x80
+};
+
+void set(uint8_t *array, unsigned int length, unsigned int position, bool value) {
+    if (position >= length * 8) {
+        std::cout << "error: set: position greater than length of array" << std::endl;
+        return;
+    }
+
+    unsigned int pos_bytes = position / 8;
+    unsigned int pos_bits = position % 8;
+
+    if (value) {
+        array[pos_bytes] = array[pos_bytes] | byte[pos_bits];
+    } else {
+        array[pos_bytes] = array[pos_bytes] & ~byte[pos_bits];
+    }
+}
+
+bool get(uint8_t *array, unsigned int length, unsigned int position) {
+    if (position >= length * 8) {
+        std::cout << "error: get: position greater than length of array" << std::endl;
+        return false;
+    }
+
+    unsigned int pos_bytes = position / 8;
+    unsigned int pos_bits = position % 8;
+    return (array[pos_bytes] & byte[pos_bits]);
+}
+
--- src/utils.h
+++ src/utils.h
@@ -3,11 +3,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdint.h>
 #include <string>
 #include <uuid/uuid.h>
+
+#define INDEX_SIZE_BITS 256
+#define INDEX_SIZE_BYTES INDEX_SIZE_BITS / 8
 
 std::string get_path(uuid_t id, std::string directory);
 std::string get_path2(uuid_t id, std::string directory);
 void create_path(std::string root_path, std::string object_path);
+void set(uint8_t *array, unsigned int length, unsigned int position, bool value);
+bool get(uint8_t *array, unsigned int length, unsigned int position);
 
 #endif // UTILS_H
